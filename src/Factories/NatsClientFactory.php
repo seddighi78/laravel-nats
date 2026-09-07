cda13a61a14ebabc6fed5e8ff891a10249956be7
Alireza Seddighi <seddighi.alireza78@gmail.com>
2026-09-07 18:06:49 +0330

    Implement client pooling

--- src/Factories/NatsClientFactory.php
+++ src/Factories/NatsClientFactory.php
@@ -5,21 +5,31 @@ namespace Seddighi78\LaravelNats\Factories;
 use Basis\Nats\Client;
 use Basis\Nats\Configuration;
 use Exception;
+use InvalidArgumentException;
 
 class NatsClientFactory implements NatsClientFactoryInterface
 {
     private array $clients = [];
+    private array $nextClient = [];
 
     public function getClient($connection = 'default'): Client
     {
-        if (isset($this->clients[$connection])) {
-            return $this->clients[$connection];
-        }
-        
         $parameters = config("nats.connections.$connection");
 
         if ($parameters === null) {
             throw new Exception("NATS Connection [$connection] not configured");
+        }
+
+        $poolSize = $parameters['pool_size'] ?? 1;
+        
+        if (!is_int($poolSize) || $poolSize < 1) {
+            throw new InvalidArgumentException("NATS Connection [$connection] pool_size must be a positive integer");
+        }
+
+        $index = ($this->nextClient[$connection] ?? 0) % $poolSize;
+        if (isset($this->clients[$connection][$index])) {
+            $this->nextClient[$connection] = ($index + 1) % $poolSize;
+            return $this->clients[$connection][$index];
         }
 
         $configuration = new Configuration([
@@ -38,8 +48,9 @@ class NatsClientFactory implements NatsClientFactoryInterface
             $configuration->setDelay($parameters['delay']['seconds'], $parameters['delay']['mode']);
         }
         
-        $this->clients[$connection] = new Client($configuration);
+        $this->clients[$connection][$index] = new Client($configuration);
+        $this->nextClient[$connection] = ($index + 1) % $poolSize;
 
-        return $this->clients[$connection];
+        return $this->clients[$connection][$index];
     }
 }
